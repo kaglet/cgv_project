5904bdc8f8bb72b72363d531e7d8c4a8335e22f4
Darth-Vallabh <amitvkesh@gmail.com>
2023-09-19 12:14:18 +0200

    add textures

--- src/script.js
+++ src/script.js
@@ -2,7 +2,10 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
-
+import woodTextureImage from './woodenfloor.jpg'; // Make sure the path to your wood texture image is correct
+import walltextureImage from './brickwall.jpg'; // Make sure the path to your wood texture image is correct
+import ceilingtextureImage from './Ceiling.jpg';
+console.log(ceilingtextureImage);
 // Debug
 const gui = new dat.GUI()
 
@@ -22,37 +25,37 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 // Create a floor tile
-const tileGeometry = new THREE.PlaneGeometry(1, 1) // 1x1 square
-const tileMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff }) // Default white color
+const tileGeometry = new THREE.PlaneGeometry(5, 5) // 1x1 square
+const tileSize = 5 // Adjust the size of each tile
+const gapSize = 0.2 // Adjust the size of the gap
 
-// Define tile size and gap size
-const tileSize = 1.2 // Adjust the size of each tile
-const gapSize = 0.01 // Adjust the size of the gap
+const floorContainer = new THREE.Group()
+const textureLoader = new THREE.TextureLoader()
+const woodTexture = textureLoader.load(woodTextureImage)
 
 
-const floorContainer = new THREE.Group();
-
+const tileMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff })
 // Duplicate tiles to create the floor with gaps
-const numRows = 10;
-const numCols = 10;
-const tiles = [];
+const numRows = 10
+const numCols = 10
+const tiles = []
 
 for (let i = 0; i < numRows; i++) {
     for (let j = 0; j < numCols; j++) {
-      const tileClone = new THREE.Mesh(tileGeometry, tileMaterial.clone());
-      const xOffset = (i - numRows / 2) * (tileSize + gapSize);
-      const yOffset = (j - numCols / 2) * (tileSize + gapSize);
-      tileClone.position.set(xOffset, yOffset, 0);
+      const tileClone = new THREE.Mesh(tileGeometry, tileMaterial.clone())
+      const xOffset = (i - numRows / 2) * (tileSize + gapSize)
+      const yOffset = (j - numCols / 2) * (tileSize + gapSize)
+      tileClone.position.set(xOffset, yOffset, 0)
   
       // Add click event listener to each tile
       tileClone.addEventListener('click', () => {
-        changeTileColorOnClick(tileClone);
-      });
+        changeTileColorOnClick(tileClone)
+      })
   
-      floorContainer.add(tileClone);
-      tiles.push(tileClone);
+      floorContainer.add(tileClone)
+      tiles.push(tileClone)
     }
-  }
+}
 
 // Function to change tile color and emit light
 function changeTileColorAndLight() {
@@ -66,31 +69,50 @@ function changeTileColorAndLight() {
 }
 
 function changeTileColorOnClick(tile) {
-    const randomColor = new THREE.Color(0,0,255);
-    tile.material.color.copy(randomColor);
+    const randomColor = new THREE.Color(0, 0, 255)
+    tile.material.color.copy(randomColor)
     const tileLight = new THREE.PointLight(randomColor, 1, 2) // Create a point light with tile color
     tileLight.position.copy(tile.position) // Position the light at the tile's position
     scene.add(tileLight)
-  }
-  
-
+}
 // Start changing tile color and emitting light every 5 seconds
 
 
 const rotationAngle = Math.PI / 2; 
 floorContainer.rotation.set(-rotationAngle, 0, 0);
 
-const translationVector = new THREE.Vector3(0.5, -5.99, -0.5); 
+const translationVector = new THREE.Vector3(0.0, -29.9, -10); 
 floorContainer.position.copy(translationVector);
 scene.add(floorContainer);
 
 
 // Create room walls
-const roomGeometry = new THREE.BoxGeometry(12, 12, 12)
-const roomMaterial = new THREE.MeshBasicMaterial({ color: 0x808080, side: THREE.BackSide }) // Gray color for the room
-const room = new THREE.Mesh(roomGeometry, roomMaterial)
+const wallTexture = textureLoader.load(walltextureImage)
+const roomGeometry = new THREE.BoxGeometry(70, 60, 80)
+const wallMaterial = new THREE.MeshStandardMaterial({ map: wallTexture, side: THREE.DoubleSide });
+const room = new THREE.Mesh(roomGeometry, wallMaterial); // Use wallMaterial for the room walls
 scene.add(room)
 
+
+// Add ceiling texture (inside the room cube)
+const ceilingTexture = textureLoader.load(ceilingtextureImage); // Load your ceiling texture image
+const ceilingMaterial = new THREE.MeshStandardMaterial({ map: ceilingTexture });
+const ceilingGeometry = new THREE.PlaneGeometry(70, 79.9);
+const ceiling = new THREE.Mesh(ceilingGeometry, ceilingMaterial);
+ceiling.position.set(0,29.99 , 0); // Adjust the position to be above the room cube
+ceiling.rotation.x = Math.PI / 2; // Rotate 90 degrees along the X-axis
+scene.add(ceiling);
+//const ceilingTexture = textureLoader.load(ceilingtextureImage); // Load your ceiling texture image
+//const ceilingMaterial = new THREE.MeshBasicMaterial({ map: ceilingTexture });
+//const ceilingGeometry = new THREE.PlaneGeometry(12, 12);
+const floorWidth = numRows * (tileSize + gapSize);
+const floorHeight = numCols * (tileSize + gapSize);
+const floorGeometry = new THREE.PlaneGeometry(70, 79.9);
+const floorMaterial = new THREE.MeshStandardMaterial({ map: woodTexture });
+const floor = new THREE.Mesh(floorGeometry, floorMaterial);
+floor.rotation.x = -Math.PI / 2; // Rotate the floor to be horizontal
+floor.position.set(0, -29.99, 0); // Set the floor position to be just below the tiles
+scene.add(floor);
 // Add lighting (point light)
 const pointLight = new THREE.PointLight(0xffffff)
 pointLight.position.set(0, 5, 0) // Adjust the position as needed
