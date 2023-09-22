26f83b4331ff83c0556efae2c54dee9f167e9b6e
Michael Kavai <mchlkavai@gmail.com>
2023-09-22 12:13:20 +0200

    Adjust tiles

--- src/js/camera.js
+++ src/js/camera.js
@@ -4,6 +4,7 @@ import { FirstPersonControls } from 'three/examples/jsm/controls/FirstPersonCont
 
 export const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.set(0, 0, 0); // Adjust camera position
+export default camera;
 
 
 
