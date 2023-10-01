4efe9c9c84646e47bf9611683cab390d706050b2
Kago Motlhabane <kmotlhabane769@gmail.com>
2023-10-01 10:21:58 +0200

    Try unsuccessfully to change far of camera

--- src/js/camera.js
+++ src/js/camera.js
@@ -1,7 +1,12 @@
 import * as THREE from 'three';
 
 // Define the first camera (you can customize its parameters)
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 30000);
+const camera = new THREE.PerspectiveCamera(
+    75, 
+    window.innerWidth / window.innerHeight, 
+    0.1, 
+    1000
+);
 camera.position.set(0, -10, 0);
 
 // Define the top-down camera (you can customize its parameters)
