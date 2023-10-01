eba3c8a54061648be0e1a2b03d634e3547dd516a
Kago Motlhabane <kmotlhabane769@gmail.com>
2023-10-01 10:45:39 +0200

    Delete old script file that affects nothing

--- src/js/camera.js
+++ src/js/camera.js
@@ -1,13 +1,13 @@
 import * as THREE from 'three';
 
 // Define the first camera (you can customize its parameters)
-const camera = new THREE.PerspectiveCamera(
+const firstPersonCamera = new THREE.PerspectiveCamera(
     75, 
     window.innerWidth / window.innerHeight, 
     0.1, 
     30000
 );
-camera.position.set(0, -10, 0);
+firstPersonCamera.position.set(0, -10, 0);
 
 // Define the top-down camera (you can customize its parameters)
 const topDownCamera = new THREE.PerspectiveCamera(
@@ -21,13 +21,13 @@ topDownCamera.position.set(0, 20, 0);
 topDownCamera.lookAt(0, 0, 0);
 
 // Initialize the current camera to be the default camera
-let currentCamera = camera;
+let currentCamera = firstPersonCamera;
 
 document.addEventListener('keydown', (event) => {
     if (event.key === 'c') {
         // Switch between cameras
-        currentCamera = (currentCamera === camera) ? topDownCamera : camera;
+        currentCamera = (currentCamera === firstPersonCamera) ? topDownCamera : firstPersonCamera;
     }
 });
 
-export { camera, topDownCamera, currentCamera };
+export { firstPersonCamera as camera, topDownCamera, currentCamera };
