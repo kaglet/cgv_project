ce32d6bfb350c5ed7de182e7b17d8ff5507b8c72
Kago Motlhabane <kmotlhabane769@gmail.com>
2023-10-14 15:23:35 +0200

    Update to see box we are in in console on c press

--- src/js/camera.js
+++ src/js/camera.js
@@ -25,16 +25,14 @@ const far = 1000;
 const topDownCamera = new THREE.OrthographicCamera(left, right, top, bottom, near, far);
 //const topDownCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-// topDownCamera.position.set(-180, 1000, -360);
-// topDownCamera.lookAt(-180, 0, -360);
+topDownCamera.position.set(-180, 1000, -360);
+topDownCamera.lookAt(-180, 0, -360);
 
 // Initialize the current camera to be the default camera
 let currentCamera = camera;
 
 document.addEventListener('keydown', (event) => {
     if (event.key === 'c') {
-
-
         levelAreas.forEach((area, index) => {
             let rangeInX = area.sizeFromBoundingBox.x / 2;
             let rangeInZ = area.sizeFromBoundingBox.z / 2;
@@ -44,15 +42,17 @@ document.addEventListener('keydown', (event) => {
 
             let inXBounds = areaWorldPosition.x - rangeInX <= player.characterModel.position.x && player.characterModel.position.x <= areaWorldPosition.x + rangeInX;
             let inZBounds = areaWorldPosition.z - rangeInZ <= player.characterModel.position.z && player.characterModel.position.z <= areaWorldPosition.z + rangeInZ;
-            
+            // box without puzzle 5
+            // box with puzzle above that is 4
+            // box with puzzle above that is 3
+            // box at other end is 0
             if (inXBounds && inZBounds) {
                 // this player is then in this square region
                 // this square has an index x
                 // can set camera based off this square's coordinates or simply from known coordinates from knowing if square 1 needs certain coordinates
                 // calculate position of topDownCamera from here
+                console.log(`I am in box ${index}`);
             }
-            console.log(`I am box ${index} with size of `);
-            console.log(area.sizeFromBoundingBox);
         });
         // Switch between cameras
         if (currentCamera === camera) {
