ef097cb2650f8fb849c6da38753d166d4214321b
Kago Motlhabane <kmotlhabane769@gmail.com>
2023-10-26 14:13:19 +0200

    Create steps array and get steps to show from after game loading is done

--- src/js/script.js
+++ src/js/script.js
@@ -21,7 +21,9 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
 
-function runGame() {
+let gameIsLoaded = false;
+
+function runGame(steps, tipDisplay) {
   const _mixers = [];
   let _previousRAF = null;
 
@@ -38,6 +40,16 @@ function runGame() {
         objects.animate_lights();
         renderer.render(objects.scene, camera.currentCamera);
         step(t - _previousRAF);
+        // if steps are not emptied out then show tips
+        if (steps.length != 0 && gameIsLoaded === true) {
+          tipDisplay.style.display = 'block';
+          if (steps[0].checkStepComplete() === true) {
+            steps.shift();
+          } else {
+            // display text for top most step
+            tipDisplay.textContent = steps[0].text;
+          }
+        }
       }
 
       _previousRAF = t;
@@ -75,6 +87,7 @@ function showLoadingScreen() {
 function hideLoadingScreen() {
   let loadingScreen = document.querySelector('.loading.screen');
   loadingScreen.style.display = 'none';
+  gameIsLoaded = true;
 }
 
 function showCrosshair() {
@@ -102,8 +115,52 @@ function _OnWindowResize() {
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
+function Step(text, checkStepComplete) {
+  this.text = text;
+  this.checkStepComplete = checkStepComplete;
+}
+
+function createTutorialSteps() {
+  let step1Text = "Press W, A, S, D to walk.";
+  let step2Text = "Walk to the front of the panel.";
+  let step3Text = "Use visual clues to find path to solve the puzzle.";
+  let step4Text = "Walk to start of puzzle: the circular tile.";
+  let step5Text = "Jump onto the start tile with spacebar.";
+  let step6Text = "Use your solution to get to the end, the semi-circular tile.";
+  let step7Text = "The door has opened indicating you completed the puzzle.";
+  
+  // when called it run setTimeout repeatedly which will run anonymous function until 10 seconds have elapsed. 
+  let checkStep1Complete = () => setTimeout(function() {
+    // Your code to execute after 10 seconds goes here
+}, 10000);
+  // check this.start and this.end for step1 as they are updated. They are initialized on construction and checked on loop run.
+  // start is filled and never filled again, end is refilled, their difference is checked.
+  let step1 =  new Step(step1Text, checkStep1Complete);
+  // let step2 =  new Step(step2Text, checkStep2Complete);
+  // let step3 =  new Step(step3Text, checkStep3Complete);
+  // let step4 =  new Step(step4Text, checkStep4Complete);
+  // let step5 =  new Step(step5Text, checkStep5Complete);
+  // let step6 =  new Step(step6Text, checkStep6Complete);
+  // let step7 =  new Step(step7Text, checkStep7Complete);
+  
+  let steps = [
+    step1,
+    // step2,
+    // step3,
+    // step4,
+    // step5,
+    // step6,
+    // step7,
+  ];
+
+  return steps;
+}
+
+let steps = createTutorialSteps();
+let tipDisplay = document.querySelector('.tip');
+
 window.addEventListener('load', () => {
-  runGame();
+  runGame(steps, tipDisplay);
   // once loading is done show start button
   const startButton = document.querySelector('.start-button');
   startButton.addEventListener('click', () => {
@@ -126,5 +183,5 @@ audioLoader.load('/Audio/BetterBackground.mp3', function (buffer) {
     sound.play();
 });
 
-document.querySelector('.pause.container resume-button')
+document.querySelector('.pause.container resume-button');
 
